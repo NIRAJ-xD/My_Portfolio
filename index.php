525c6ec806a2ead61119ef23ba9bba30a5857fe6
NIRAJ-xD <itznirajgour@gmail.com>
2025-05-07 21:57:26 +0530

    Css

--- index.php
+++ index.php
@@ -6,15 +6,19 @@
     <title>My Portfolio</title>
     <link rel="icon" href="icon.png" type="image/png">
     <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="container">
-        <form action="">
-            <h1 class="heading"> Portfolio</h1>
+            <h1 class="heading">Hello, i'm Niraj Gour.</h1>
+            <img src="photo.jpeg" id="profile" alt="profile" class="d-block ms-auto mb-3">
             <hr>
-            <h2 class="sub_heading">Introduction</h2>
+            <h2 class="sub_heading">I'm a Web Developer | Python, Java & PHP Enthusiast</h2>
             <hr>
-        </form>
+            <p>build responsive websites and enjoy solving coding problems</p>
+            <hr>
+            <a href="resume.pdf" class="btn btn-primary me-2" download>Download Resume</a>
+            <a href="#Contact" class="btn btn-outline-primary">Hire Me</a>
     </div>
 </body>
 </html>
