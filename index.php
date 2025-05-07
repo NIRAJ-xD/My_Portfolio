86fb0aa02affdf2896cffbbda98d7263b6eebd39
NIRAJ-xD <itznirajgour@gmail.com>
2025-05-07 20:06:03 +0530

    editing

--- index.php
+++ index.php
@@ -4,12 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Portfolio</title>
+    <link rel="icon" href="icon.png" type="image/png">
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <h1> MY PORTFOLIO</h1>
-    <div>
-        <input type="button"value="search">
+    <div class="container">
+        <form action="">
+            <h1 class="heading"> Portfolio</h1>
+            <hr>
+            <h2 class="sub_heading">Introduction</h2>
+            <hr>
+        </form>
     </div>
 </body>
 </html>
