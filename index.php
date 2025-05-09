d0557e7f54f29120e58cd7e1b66807b0a18c633a
NIRAJ-xD <itznirajgour@gmail.com>
2025-05-09 12:12:00 +0530

    More Editing

--- index.php
+++ index.php
@@ -7,18 +7,46 @@
     <link rel="icon" href="icon.png" type="image/png">
     <link rel="stylesheet" href="styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
 </head>
-<body>
-    <div class="container">
-            <h1 class="heading">Hello, i'm Niraj Gour.</h1>
-            <img src="photo.jpeg" id="profile" alt="profile" class="d-block ms-auto mb-3">
-            <hr>
-            <h2 class="sub_heading">I'm a Web Developer | Python, Java & PHP Enthusiast</h2>
-            <hr>
-            <p>build responsive websites and enjoy solving coding problems</p>
-            <hr>
-            <a href="resume.pdf" class="btn btn-primary me-2" download>Download Resume</a>
-            <a href="#Contact" class="btn btn-outline-primary">Hire Me</a>
-    </div>
+<body class="full">
+    <header>
+        <nav>
+        <ul class="nav-links">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Portfolio</a></li>
+        </ul>
+        </nav>
+    </header>
+    <section class="hero">
+        <div class="hero-content">
+            <div class="hero-text">
+                <h1>Hello, I'm Niraj Gour</h1>
+                <img src="photo.jpeg" class="image" alt="Niraj's photo">
+                <p>A Web Developer | Python, Java & PHP</p>
+                <p>Enthusiast</p>
+            </div>
+        </div>
+    </section>
+    <section class="about">
+        <p>
+         Hi! I'm Niraj Gour, a passionate and self-motivated web developer currently learning and building projects in HTML, CSS, JavaScript,   and PHP. I enjoy turning complex problems into simple, beautiful, and intuitive designs.
+        </p>
+        <p>
+        I have a strong foundation in Python and Java, and I’m currently focused on full-stack web development. I love building responsive  websites, exploring backend technologies, and solving coding challenges.
+        <div>
+            <button> Download Resume</button>
+            <button> Hire ME</button>
+        </div>
+    </section>
+    <section class="socials">
+        <a href="https://www.youtube.com/@BladeXD_gg" target="_blank"><i class="fab fa-youtube"></i></a>
+        <a href="https://github.com/NIRAJ-xDS" target="_blank"><i class="fab fa-github"></i></a>
+        <p>Get in touch</p>
+    </section>
+    <footer>
+        <p>© 2025 Niraj Gour. Built with ❤️ JAVA, CSS & HTML</p>
+    </footer>
 </body>
 </html>
